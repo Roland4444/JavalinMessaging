60e94247e809ac51460ac2462ffc8a8c9fb328f2
Roland4444 <turbopollux@rambler.ru>
2018-10-16 10:51:13 +0000

    succesfull send binary ar raw POST

--- src/main/scala/Receiver.scala
+++ src/main/scala/Receiver.scala
@@ -3,13 +3,20 @@ import java.net.http.{HttpClient, HttpRequest}
 import java.net.http.HttpClient.Version
 import io.javalin.{Context, Handler, Javalin}
 object Receiver {
+  def valueOf(buf: Array[Byte]): String = buf.map("%02X" format _).mkString
+
   val readMessage: Handler = new Handler {override def handle(context: Context) ={
     println("GETING REQUEST")
     context.html("received");
   }}
+
+  val writetoFile: Handler = new Handler {override def handle(context: Context) ={
+    println(valueOf(context.bodyAsBytes()))
+  }}
   def main(args: Array[String]): Unit = {
     var app: Javalin = Javalin.create.start(8888)
     app.get("/", readMessage)
+    app.post("/", writetoFile)
   }
 }
 
--- src/main/scala/Sender.scala
+++ src/main/scala/Sender.scala
@@ -1,15 +1,26 @@
 import java.net.URI
 import java.net.http.{HttpClient, HttpRequest}
 import java.net.http.HttpClient.Version
+import java.net.http.HttpRequest.{BodyPublisher, BodyPublishers}
+import java.nio.ByteBuffer
+import java.util.concurrent.Flow
+
 import io.javalin.{Context, Handler, Javalin}
 object Sender {
+  val arr = Array[Byte](0xaa.toByte, 0xaa.toByte, 0x55.toByte, 0x55.toByte)
   val httpClient = HttpClient.newBuilder()
-    .version(Version.HTTP_2)  
+    .version(Version.HTTP_2)
     .build();
   val server = "http://127.0.0.1:8888/";
   val request = HttpRequest.newBuilder()
     .uri(URI.create(server))
     .GET()
+    .build();
+
+
+  val binaryReq = HttpRequest.newBuilder()
+    .uri(URI.create(server))
+    .POST(BodyPublishers.ofByteArray(arr))
     .build();
   val sendMessage: Handler = new Handler {override def handle(context: Context) ={
     import java.net.http.HttpResponse.BodyHandlers
@@ -19,8 +30,19 @@ object Sender {
       println("Response body: " + response.body)
     })
   }}
+
+  val sendBinaryViaPost: Handler = new Handler {override def handle(context: Context) ={
+    println("SEND BINARY!")
+    import java.net.http.HttpResponse.BodyHandlers
+    httpClient.sendAsync(binaryReq, BodyHandlers.ofString).thenAccept((response) => {
+      println("Response status code: " + response.statusCode)
+      println("Response headers: " + response.headers)
+      println("Response body: " + response.body)
+    })
+  }}
   def main(args: Array[String]): Unit = {
     var app: Javalin = Javalin.create.start(5555)
     app.get("/", sendMessage)
+    app.get("/sb", sendBinaryViaPost)
   }
 }
